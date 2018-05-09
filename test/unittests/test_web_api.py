27b6919aa18b432bb9e1e18bceb3cad98bd8c120
Fledermann <37776145+Fledermann@users.noreply.github.com>
2018-05-09 17:32:27 +0200

    +test case

--- test/unittests/test_web_api.py
+++ test/unittests/test_web_api.py
@@ -5,11 +5,18 @@ convienience, this module shouldn't be run directly, use the runner instead.
 """
 
 import unittest
+from unittest import mock
 
 from paletti import web_api
 
 
 class TestWebAPI(unittest.TestCase):
+
+    def test_download(self):
+        web_api.streams = mock.Mock(return_value={'title': 'mock'})
+        web_api.metadata = mock.Mock(return_value={'title': 'mock'})
+        web_api.Download = mock.Mock()
+        self.assertIsInstance(web_api.download('https://example.com', '/tmp'), mock.Mock)
 
     def test_user(self):
         # The result should be a dict.
