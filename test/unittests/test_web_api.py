3b4992b2719f10b6a94c932a6b88c86126111ae4
Fledermann <37776145+Fledermann@users.noreply.github.com>
2018-05-10 09:52:37 +0200

    +web_api tests

--- test/unittests/test_web_api.py
+++ test/unittests/test_web_api.py
@@ -4,6 +4,7 @@
 convienience, this module shouldn't be run directly, use the runner instead.
 """
 
+import importlib
 import unittest
 from collections import namedtuple
 from unittest import mock
@@ -13,30 +14,40 @@ from paletti import web_api
 
 class TestWebAPI(unittest.TestCase):
 
+    def setUp(self):
+        # We need some set up to mock the decorators appropiately.
+        # Throughout these tests, our mocked plugin will be called
+        # 'cool_plugin' and its website is 'http://example.com'
+        mod_file = namedtuple('mod_file', ['name'])
+        module = namedtuple('module', ['HOSTS', 'STREAM_TYPE'])
+        cool_plugin = mod_file(name='cool_plugin')
+        mod = module(HOSTS=['example.com'], STREAM_TYPE='audio+video')
+        mock_pkgs = [cool_plugin]
+        web_api.pkgutil.walk_packages = mock.Mock(return_value=mock_pkgs)
+        web_api.importlib.import_module = mock.Mock(return_value=mod)
+
+        def kill_patches():
+            mock.patch.stopall()
+            importlib.reload(web_api)
+
+        self.addCleanup(kill_patches)
+        mock.patch('paletti.web_api.module', lambda x: x).start()
+        importlib.reload(web_api)
+
     def test_cache(self):
         @web_api.cache
         def f(plugin, testitem):
             return {'url': testitem}
-        self.assertIsInstance(f('foo', 'http://bar'), dict)
-        self.assertIsInstance(f('foo', 'http://bar'), dict)
+        self.assertIsInstance(f('foo', 'http://example.com/213'), dict)
+        self.assertIsInstance(f('foo', 'http://example.com/123'), dict)
 
     def test_module(self):
-        mod_file = namedtuple('mod_file', ['name'])
-        module = namedtuple('module', ['HOSTS', 'STREAM_TYPE'])
-        cool_plugin = mod_file(name='cool_plugin')
-        mod = module(HOSTS=['b.foo.com'], STREAM_TYPE='audio+video')
-        mock_pkgs = [cool_plugin]
-
-        web_api.pkgutil.walk_packages = mock.Mock(return_value=mock_pkgs)
-        web_api.importlib.import_module = mock.Mock(return_value=mod)
-
         @web_api.module
         def f(plugin, testitem):
             return {'url': 'testitem'}
-        self.assertIsInstance(f('cool_plugin', 'http://bar'), dict)
-        self.assertIsInstance(f('cool_plugin', 'http://bar'), dict)
-        self.assertRaises(ModuleNotFoundError, lambda: f('no_plugin', 'http://bar'))
-
+        self.assertIsInstance(f('cool_plugin', 'search query'), dict)
+        self.assertIsInstance(f('http://example.com/123'), dict)
+        self.assertRaises(ModuleNotFoundError, lambda: f('no_plugin', '-'))
 
     def test_download(self):
         web_api.streams = mock.Mock(return_value={'title': 'mock'})
@@ -44,6 +55,15 @@ class TestWebAPI(unittest.TestCase):
         web_api.Download = mock.Mock()
         self.assertIsInstance(web_api.download('https://example.com', '/tmp'), mock.Mock)
 
+    def test_thumbnail(self):
+        # TODOD: mock file write.
+        md = {'id': '12345', 'thumbnail_small': 'http://example.com/thumb.jpg'}
+        web_api.metadata = mock.Mock(return_value=md)
+        web_api.urllib3.PoolManager = mock.Mock()
+        web_api.urllib3.PoolManager.return_value.request.return_value.data = b'12345'
+
+        self.assertEqual(web_api.thumbnail('http://example.com/123'), '/tmp/12345.jpg')
+
     def test_user(self):
-        # The result should be a dict.
-        self.assertRaises(ModuleNotFoundError, lambda: web_api.user('foo', 'bar'))
+        url = 'http://example.com/123'
+        self.assertRaises(NotImplementedError, lambda: web_api.user(url))
